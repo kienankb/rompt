57a858dc2823ac0b9a6c5841cb8d509565faccff
Kienan Knight-Boehm <kienan@kienankb.com>
2022-09-15 13:04:25 -0700

    get cwd path using stdlib instead

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let username = &args[1];
-    let cwd = &args[2];
-    print!("{}  {}  ", username, cwd);
+    let cwd = env::current_dir().unwrap();
+    let cwd_str = cwd.to_str().expect("failed to get current working directory").split("/");
+    print!("{}  ", username);
+    for path_frag in cwd_str {
+        print!("{}  ", path_frag);
+    }
     io::stdout().flush().unwrap();
 }
